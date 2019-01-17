bc67aeae81e54e0dd99e9289fd3186a0154b3e03
Hugo Virgen <virgenherrera@gmail.com>
2019-01-17 15:49:44 -0600

    changelog
    - shrink package.json
    - install @tne/express-app
    - install mongoose
    - init service

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-// service bootstrap here!
+import { ExpressApplication, IAppSettings } from '@tne/express-app';
+import { postLoader } from './lib/postLoader';
+
+const { name } = require('../package.json');
+const maxIncomingReq = '2mb';
+const config: IAppSettings = {
+	appPath: __dirname,
+	appName: name,
+	urlEncodedOptions: {
+		limit: maxIncomingReq,
+		extended: true
+	},
+	jsonOptions: {
+		limit: maxIncomingReq,
+		strict: true,
+	},
+	preRouteHooks: [],
+	publicFolder: '../public',
+	faviconPath: '../public/favicon.ico',
+	// routesFolder: './Handler/Restful',
+};
+
+(postLoader)
+	(ExpressApplication.construct(config));
